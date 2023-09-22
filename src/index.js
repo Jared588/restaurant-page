4097067ecec2281178ef53125dad5728afe908dd
Jared588 <jaredeprice@gmail.com>
2023-09-22 15:18:28 +0200

    Add basic grid layout

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './styles.css';
+import Home from './home';
 
-console.log("working!");
+function initialize() {
+    const header = document.createElement('header');
+    const main = document.createElement('div'); 
+    const footer = document.createElement('footer');
+    
+    header.classList.add("header");
+    main.classList.add("main");
+    footer.classList.add("footer");
+
+    document.body.appendChild(header);
+    document.body.appendChild(main);
+    document.body.appendChild(footer);
+}
+
+initialize();
